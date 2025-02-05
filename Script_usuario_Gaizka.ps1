84c2af3ec484f7243775541aa565abc02455bf96
Gaizka <gaizkagarralda@gmail.com>
2025-02-05 14:22:14 +0100

    examen

---
+++ Script_usuario_Gaizka.ps1
@@ -0,0 +1,10 @@
+﻿    New-LocalUser -Name "usu1examen" -Password (ConvertTo-SecureString "Password0" -AsPlainText -Force) -FullName "usu1examen" -Description "Usuario creado"
+    New-LocalUser -Name "usu2examen" -Password (ConvertTo-SecureString "Password0" -AsPlainText -Force) -FullName "usu2examen" -Description "Usuario creado"
+
+
+    New-Item -Path "C:\Users\usu1examen" -ItemType Directory
+    New-Item -Path "C:\Users\usu2examen" -ItemType Directory
+
+    
+    Set-LocalUser -Name "usu1examen" -AccountNeverExpires
+    Set-LocalUser -Name "usu2examen" -AccountNeverExpires
